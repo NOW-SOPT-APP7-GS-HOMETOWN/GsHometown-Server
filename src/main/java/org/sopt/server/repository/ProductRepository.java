91257707abd69db4440e36d7133c9fc821be5cc5
nykoh2001 <nykoh2001@gmail.com>
2024-05-15 19:02:41 +0900

    [FEAT] add repositories #4

---
+++ src/main/java/org/sopt/server/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package org.sopt.server.repository;
+
+import java.util.Optional;
+import org.sopt.server.domain.Product;
+import org.sopt.server.exception.CommonException;
+import org.sopt.server.exception.dto.ErrorCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    default Product findByIdOrThrow(final Long productId) {
+        return findById(productId).orElseThrow(() -> new CommonException(ErrorCode.PRODUCT_NOT_FOUND));
+    }
+
+    Optional<Product> findById(final Long productId);
+}
