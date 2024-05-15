6064584749bf8ef9cba0531833ccb80c3de31f68
nykoh2001 <nykoh2001@gmail.com>
2024-05-15 19:25:51 +0900

    [CHORE] remove useless import #4

--- src/main/java/org/sopt/server/domain/Product.java
+++ src/main/java/org/sopt/server/domain/Product.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AccessLevel;
--- src/main/java/org/sopt/server/domain/ProductDetail.java
+++ src/main/java/org/sopt/server/domain/ProductDetail.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
