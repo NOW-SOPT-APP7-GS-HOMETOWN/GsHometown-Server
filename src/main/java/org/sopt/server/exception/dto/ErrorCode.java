fb1cea5a4b7ced091bccf8787f2e375fe977c715
nykoh2001 <nykoh2001@gmail.com>
2024-05-15 19:03:42 +0900

    [CHORE] add error code #4

--- src/main/java/org/sopt/server/exception/dto/ErrorCode.java
+++ src/main/java/org/sopt/server/exception/dto/ErrorCode.java
@@ -9,12 +9,28 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorCode {
 
+    /* 400 - BAD REQUEST */
     BAD_REQUEST(40000, HttpStatus.BAD_REQUEST, "Bad Request"),
+
+    /* 401 - UNAUTHORIZED */
     UNAUTHORIZED(40100, HttpStatus.UNAUTHORIZED, "Unauthorized"),
+
+    /* 403 - FORBIDDEN */
     FORBIDDEN(40300, HttpStatus.FORBIDDEN, "Forbidden"),
+
+    /* 404 - NOT FOUND */
     NOT_FOUND(40400, HttpStatus.NOT_FOUND, "Not Found"),
+    MEMBER_NOT_FOUND(40401, HttpStatus.NOT_FOUND, "Member Not Found"),
+    PRODUCT_NOT_FOUND(40402, HttpStatus.NOT_FOUND, "Product Not Found"),
+    PRODUCT_DETAIL_NOT_FOUND(40403, HttpStatus.NOT_FOUND, "Product Detail Not Found"),
+
+    /* 405 - METHOD NOT ALLOWED */
     METHOD_NOT_ALLOWED(40500, HttpStatus.METHOD_NOT_ALLOWED, "Method Not Allowed"),
+
+    /* 409 - CONFLICT */
     CONFLICT(40900, HttpStatus.CONFLICT, "Conflict"),
+
+    /* 500 - INTERNAL SERVER ERROR */
     INTERNAL_SERVER_ERROR(50000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
     ;
 
