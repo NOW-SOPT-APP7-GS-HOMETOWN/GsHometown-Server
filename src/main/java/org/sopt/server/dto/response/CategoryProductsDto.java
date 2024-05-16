beb044ad568235c5da48697ac65b12da7e5f7d41
nykoh2001 <nykoh2001@gmail.com>
2024-05-16 19:34:32 +0900

    [FIX] return category as string #7

--- src/main/java/org/sopt/server/dto/response/CategoryProductsDto.java
+++ src/main/java/org/sopt/server/dto/response/CategoryProductsDto.java
@@ -4,10 +4,10 @@ import java.util.List;
 import org.sopt.server.domain.type.Category;
 
 public record CategoryProductsDto(
-        Category category,
+        String category,
         List<ProductDto> products
 ) {
     public static CategoryProductsDto of(final Category category, final List<ProductDto> products) {
-        return new CategoryProductsDto(category, products);
+        return new CategoryProductsDto(category.getCategory(), products);
     }
 }
